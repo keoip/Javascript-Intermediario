b52a80337ad466360827d85982fd7fced9f54c26
Kelvin melo <kelvinhenriquemelo@gmail.com>
2024-07-29 15:36:42 -0300

    comited

--- listaexercicio3.js
+++ listaexercicio3.js
@@ -1,13 +1,15 @@
 let precoNormalEtiquetaIphone = 3500;
-let opcaoDePagamento = '';
+let opcaoDePagamento = 'mais de duas vezes';
 
 if( opcaoDePagamento == 'debito'){
-    precoNormalEtiquetaIphone - 350
+    let debito = precoNormalEtiquetaIphone - 350
+    console.log(`O Preço final da comptra feita no debito é de ${debito}`)
 } else if(opcaoDePagamento == 'vista no dinheiro' || opcaoDePagamento == 'Pix'){
-    precoNormalEtiquetaIphone - 525
+    let vista = precoNormalEtiquetaIphone - 525
+    console.log(`O Preço final da compra a vista no dinheiro ou no Pix é de ${vista}`)
 } else if(opcaoDePagamento == 'Duas vezes'){
     let duasVezes = precoNormalEtiquetaIphone
-    console.log(`O Preço final da compra feita em duas vezes é de ${precoNormalEtiquetaIphone}`)
+    console.log(`O Preço final da compra feita em duas vezes é de ${duasVezes}`)
 } else if(opcaoDePagamento == 'mais de duas vezes'){
     let maisDeDuasVezes = precoNormalEtiquetaIphone + 350
     console.log(`O Preço final da compra do produto feita em mais de duas vezes é de ${maisDeDuasVezes}`)
