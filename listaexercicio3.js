e503fe1fda4340d565f09949fd5a2df706d90e1f
Kelvin melo <kelvinhenriquemelo@gmail.com>
2024-07-30 22:23:06 -0300

    comited

--- listaexercicio3.js
+++ listaexercicio3.js
@@ -1,16 +1,18 @@
-let precoNormalEtiquetaIphone = 3500;
-let opcaoDePagamento = 'mais de duas vezes';
 
-if( opcaoDePagamento == 'debito'){
-    let debito = precoNormalEtiquetaIphone - 350
-    console.log(`O Preço final da comptra feita no debito é de ${debito}`)
-} else if(opcaoDePagamento == 'vista no dinheiro' || opcaoDePagamento == 'Pix'){
-    let vista = precoNormalEtiquetaIphone - 525
-    console.log(`O Preço final da compra a vista no dinheiro ou no Pix é de ${vista}`)
-} else if(opcaoDePagamento == 'Duas vezes'){
-    let duasVezes = precoNormalEtiquetaIphone
-    console.log(`O Preço final da compra feita em duas vezes é de ${duasVezes}`)
-} else if(opcaoDePagamento == 'mais de duas vezes'){
-    let maisDeDuasVezes = precoNormalEtiquetaIphone + 350
-    console.log(`O Preço final da compra do produto feita em mais de duas vezes é de ${maisDeDuasVezes}`)
+ let pagamento = (precoEtiquetaIphone, pagamentos )=>{
+    if( pagamentos == 1){
+        let debito = precoEtiquetaIphone - 350
+        console.log(`O Preço final da comptra feita no debito é de ${debito}`)
+    } else if(pagamentos == 2 || pagamentos == 2){
+        let vista = precoEtiquetaIphone - 525
+        console.log(`O Preço final da compra a vista no dinheiro ou no Pix é de ${vista}`)
+    } else if(pagamentos == 3){
+        let duasVezes = precoEtiquetaIphone
+        console.log(`O Preço final da compra feita em duas vezes é de ${duasVezes}`)
+    } else if(pagamentos == 4){
+        let maisDeDuasVezes = precoEtiquetaIphone + 350
+        console.log(`O Preço final da compra do produto feita em mais de duas vezes é de ${maisDeDuasVezes}`)
+    }
 }
+
+pagamento(3500, 1);
